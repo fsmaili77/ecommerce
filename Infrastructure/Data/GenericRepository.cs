2ab84af777933f783bfaeebf7429fd730879d574
Fehmir <fehmir@gmail.com>
2023-07-20 12:23:50 +0200

    API - Orders 2 (Creating order service and order controller; Adding Unit of Work; AutoMapper config for orders; AutoMapper value Resolver)

--- Infrastructure/Data/GenericRepository.cs
+++ Infrastructure/Data/GenericRepository.cs
@@ -13,7 +13,7 @@ namespace Infrastructure.Data
         {
             _context = context;
         }
-
+        
         public async Task<int> CountAsync(ISpecification<T> spec)
         {
             return await ApplySpecification(spec).CountAsync();
@@ -38,10 +38,26 @@ namespace Infrastructure.Data
         {
             return await ApplySpecification(spec).ToListAsync();
         }
-
+        
         private IQueryable<T> ApplySpecification(ISpecification<T> spec)
         {
             return SpecificationEvaluator<T>.GetQuery(_context.Set<T>().AsQueryable(), spec);
+        }
+
+        public void Add(T entity)
+        {
+            _context.Set<T>().Add(entity);
+        }
+
+        public void Update(T entity)
+        {
+             _context.Set<T>().Attach(entity);
+             _context.Entry(entity).State = EntityState.Modified;
+        }
+
+         public void Delete(T entity)
+        {
+             _context.Set<T>().Remove(entity);
         }
     }
 }
